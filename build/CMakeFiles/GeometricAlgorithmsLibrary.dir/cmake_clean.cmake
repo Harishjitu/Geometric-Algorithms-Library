cb4a21e528051c0d5a959c0c63d9518965f63a46
Harishjitu Saseendran <jitusaseendran@gmail.com>
2024-06-03 16:11:46 -0400

    Initial Commit

---
+++ build/CMakeFiles/GeometricAlgorithmsLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GeometricAlgorithmsLibrary.dir/src/main.cpp.o"
+  "CMakeFiles/GeometricAlgorithmsLibrary.dir/src/main.cpp.o.d"
+  "GeometricAlgorithmsLibrary"
+  "GeometricAlgorithmsLibrary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GeometricAlgorithmsLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
